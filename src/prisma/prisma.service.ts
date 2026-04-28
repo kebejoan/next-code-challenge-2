481aa04776595e22c4c65cd09e729e92343dbf24
kebejoan <jmailed.joe@gmail.com>
2026-04-28 20:45:50 +0700

    fix(prisma): fix .env cant be found and can't initialize prisma

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,5 +1,7 @@
 import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
+import { Pool } from 'pg';
 
 @Injectable()
 export class PrismaService
@@ -8,6 +10,16 @@ export class PrismaService
 {
   // In V7, we can leave the constructor empty or remove it entirely
   // Prisma will automatically look for DATABASE_URL in your process.env
+
+  constructor() {
+    super({
+      adapter: new PrismaPg(
+        new Pool({
+          connectionString: process.env.DATABASE_URL,
+        }),
+      ),
+    });
+  }
 
   async onModuleInit() {
     // If the URL is missing, provide a helpful error message in the console
