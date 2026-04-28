d50adea8be7b4ff882805821a97af7d1fc4b2434
kebejoan <jmailed.joe@gmail.com>
2026-04-28 21:40:31 +0700

    feat(jwt): implement jwt strategy

---
+++ src/auth/jwt.strategy.ts
@@ -0,0 +1,23 @@
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { PassportStrategy } from '@nestjs/passport';
+import { Injectable } from '@nestjs/common';
+import { Payload } from './interface';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor() {
+    const secret = process.env.JWT_SECRET;
+    if (!secret) {
+      throw new Error('JWT_SECRET is not defined in environment variables');
+    }
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: secret,
+    });
+  }
+
+  async validate(payload: Payload) {
+    return { userId: payload.sub, email: payload.email };
+  }
+}
